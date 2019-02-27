be9bdd1687944f7da0e603b621004a88fe61858d
mohamedsl22 <moodi.salman@gmail.com>
2019-02-27 02:19:02 +0200

    27.2

--- src/app/src/main/java/com/example/moodisalman/subitizing/GameScreen.java
+++ src/app/src/main/java/com/example/moodisalman/subitizing/GameScreen.java
@@ -2,6 +2,8 @@ package com.example.moodisalman.subitizing;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
@@ -14,39 +16,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 public class GameScreen extends AppCompatActivity {
     private GameView game;
     private Button btn1;
+    private VideoView videoView;
+    private Uri ur;
+    private int curLevel,numOfWin=0,numOfLose=0 , tmpLevel;
+    private final int DISAPPEAR_OBJECTS=-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
-        video.v[0] = findViewById(R.id.videoView);
+        videoView= findViewById(R.id.videoView);
         btn1 = findViewById(R.id.button);
 
-//        if (video.isRunning == false) {
-//            video.isRunning = true;
-//            startService(new Intent(this, MyService.class));
-//
-//
-//        }
-        game = new GameView(this);
+        /** for the background video**/
 
-        print(video.level + "");
-
-        btn1.setOnClickListener(new View.OnClickListener() {
+        ur= Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.underwater1);
+        videoView.setVideoURI(ur);
+        videoView.start();
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
-            public void onClick(View view) {
-                Log.d("debuging", "level = " + video.level);
-                AnswersDialog();
-                video.level++;
-                print(video.level + "");
-                if (video.level >= 9)
-                    video.level = 1;
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
             }
         });
+        /** **/
 
+        game = new GameView(this);
 
         ConstraintLayout layout = findViewById(R.id.layout);
 
@@ -54,47 +53,24 @@ public class GameScreen extends AppCompatActivity {
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int height = displayMetrics.heightPixels;
         int width = displayMetrics.widthPixels;
-
         layout.addView(game, width - 1, height - 1);
 
 
-//        new CountDownTimer(30000, 1000) {
-//
-//            public void onTick(long millisUntilFinished) {
-////                mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
-//                AnswersDialog();
-//                video.level++;
-//                if(video.level>=9)
-//                    video.level=0;
-//
-//            }
-//
-//            public void onFinish() {
-////                mTextField.setText("done!");
-//            }
-//        }.start();
-//            new Handler().postDelayed(new Runnable() {
-//                @Override
-//                public void run() {
-//                    showAlertDialogButtonClicked(game);
-//
-//                }
-//
-//            }, 3000);
-            timer();
-//        final Handler handler = new Handler();
-//        final int delay = 3000; //milliseconds
-//
-//        handler.postDelayed(new Runnable(){
-//            public void run(){
-//                //do something
-//                showAlertDialogButtonClicked(game);
-//                handler.postDelayed(this, delay);
-//            }
-//        }, delay);
-//            video.level++;
-//            if (video.level >= 9)
-//                video.level = 1;
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                print("level= "+video.level);
+                video.level++;
+                print(video.level + "");
+                if (video.level > 9)
+                    video.level = 1;
+            }
+        });
+            print("main level= "+video.level);
+            gameProcess();
+
+//        timer();
+
 
     }
     public void showAlertDialogButtonClicked(View view) {
@@ -105,54 +81,123 @@ public class GameScreen extends AppCompatActivity {
         View view1 =inflater.inflate(R.layout.activity_answers,null);
         builder.setView(view1).setTitle("תשובות");
 
+//        print("dialog level= "+video.level);
+
         String[] tmpAns=new String[4];
-        if (video.level<=4){
+        if (tmpLevel==1){
             for (int i=0;i<tmpAns.length;i++)
                 tmpAns[i]=i+1+"";
         }
-        else{
-            for (int i=0;i<tmpAns.length;i++){
-                tmpAns[i]=video.level-i+"";
-            }
+        else if(tmpLevel>=2 && tmpLevel<=5){
+            for (int i=0;i<tmpAns.length;i++)
+                tmpAns[i]=i+2+"";
+        }
+        else {
+            for (int i=0;i<tmpAns.length;i++)
+                tmpAns[i]=i+6+"";
         }
 
         // add a list
         builder.setItems(tmpAns, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                which++;
+                which=tmpLevel;
                 switch (which) {
                     case 1:
-                        if (which==video.level)
-                            print("Well done!");
-                        else
-                            print(which+" "+video.level+" 11111");
-                        timer();
+                        if (which==tmpLevel){
+                            print("Well done!\uD83C\uDF89\uD83C\uDF89");
+                            numOfWin++;
+                        }
+                        else{
+                            print(which+" "+curLevel+" 11111");
+                        }
                         break;
+
                     case 2:
-                        if (which==video.level)
-                        print("Well done!");
+                        if (which==tmpLevel){
+                            print("Well done!\uD83C\uDF89\uD83C\uDF89");
+                            numOfWin++;
+                        }
                     else
-                        print(which+"");
-                    timer();
+                        print(video.level+" "+curLevel);
                         break;
                     case 3:
-                        if (which==video.level)
-                            print("Well done!");
+                        if (which==tmpLevel){
+                            print("Well done!\uD83C\uDF89\uD83C\uDF89");
+                            numOfWin++;
+                        }
+                        else
+                            print(which+"");
+                        break;
+                    case 4:
+                        if (which==tmpLevel){
+                            print("Well done!\uD83C\uDF89\uD83C\uDF89");
+                            numOfWin++;
+                        }
+                        else
+                            print(which+"");
+                        break;
+                    case 5:
+                        if (which==tmpLevel){
+                            print("Well done!\uD83C\uDF89\uD83C\uDF89");
+                            numOfWin++;
+                        }
+                        else
+                            print(which+"");
+                        break;
+                    case 6:
+                        if (which==tmpLevel){
+                            print("Well done!\uD83C\uDF89\uD83C\uDF89");
+                            numOfWin++;
+                        }
+                        else
+                            print(which+"");
+                        break;
+                    case 7:
+                        if (which==tmpLevel){
+                            print("Well done!\uD83C\uDF89\uD83C\uDF89");
+                            numOfWin++;
+                        }
+                        else
+                            print(which+"");
+                        break;
+                    case 8:
+                        if (which==tmpLevel){
+                            print("Well done!\uD83C\uDF89\uD83C\uDF89");
+                            numOfWin++;
+                        }
+                        else
+                            print(which+"");
+                        break;
+                    case 9:
+                        if (which==tmpLevel){
+                            print("Well done!\uD83C\uDF89\uD83C\uDF89");
+                            numOfWin++;
+                        }
                         else
                             print(which+"");
                         break;
                 }
+                video.level=curLevel;
+                if (numOfWin==3){
+                    video.level++;
+                    print("vid level= "+video.level);
+                    numOfWin=0;
+                    if (video.level==10){}////end of the game.
+                }
+
+                gameProcess();
+
             }
         });
         AlertDialog dialog = builder.create();
         dialog.show();
     }
 
-    public void AnswersDialog(){
-        Answers answers=new Answers();
-        answers.show(getSupportFragmentManager(),"answers dialog");
-    }
+//    public void AnswersDialog(){
+//        Answers answers=new Answers();
+//        answers.show(getSupportFragmentManager(),"answers dialog");
+//    }
 
     private void print(String s){
         Toast.makeText(this, s,
@@ -160,16 +205,45 @@ public class GameScreen extends AppCompatActivity {
     }
 
     private void timer(){
+//        print("timer level= "+video.level);
         final Handler handler = new Handler();
         final int delay = 3000; //milliseconds
-
         handler.postDelayed(new Runnable(){
             public void run(){
                 //do something
+//                curLevel=video.level;
+                video.level=DISAPPEAR_OBJECTS;
                 showAlertDialogButtonClicked(game);
 //                handler.postDelayed(this, delay);
             }
         }, delay);
+
     }
+
+    public void gameProcess(){
+//        print("game lvel= "+video.level);
+        if (video.level<=2){
+            tmpLevel=video.level;
+            curLevel= video.level;
+            timer();
+        }
+        else{
+            if (numOfWin==0){
+                curLevel=video.level;
+                tmpLevel=video.level;
+                timer();
+            }
+            else{
+                curLevel=video.level;//to choose a level randomly and save main level in tmplevel
+                int rnd=(int)(Math.random()*video.level)+1;
+                print("rnd= "+rnd);
+                video.level=rnd;
+                tmpLevel=rnd;
+                timer();
+            }
+
+        }
+    }
+
 
 }
--- src/app/src/main/java/com/example/moodisalman/subitizing/GameView.java
+++ src/app/src/main/java/com/example/moodisalman/subitizing/GameView.java
@@ -9,16 +9,17 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.view.View;
 
+import java.util.Vector;
+
 
 public class GameView extends View  {
     private Bitmap obj;
+    private Rect rect[];
 
-    public GameView(Context context) {
+    public GameView(Context context) {//constructor
         super(context);
 
         obj= BitmapFactory.decodeResource(getResources(),R.drawable.fish);
-
-
 
     }
 
@@ -33,56 +34,124 @@ public class GameView extends View  {
         super.onDraw(canvas);
         int level=video.level;
 
-        //Rect rect = new Rect(1/5* canvas.getWidth(), 2/5* canvas.getHeight(), (1/5 + canvas.getWidth()/5 ),(2/5 + canvas.getHeight()/5 ));
-
-        Rect rect = new Rect(canvas.getWidth()/2+200, canvas.getHeight()/2+200, canvas.getWidth()/2+300,canvas.getHeight()/2+300);
-
-        Rect rect1 = new Rect(canvas.getWidth()/2, canvas.getHeight()/2, canvas.getWidth()/2+100,canvas.getHeight()/2+100);
-        //canvas.drawBitmap(obj, null, rect1, null);
-        Rect rect2 = new Rect(canvas.getWidth()/2+150, canvas.getHeight()/2, canvas.getWidth()/2+250,canvas.getHeight()/2+100);
-
-        Rect rect3 = new Rect(canvas.getWidth()/2, canvas.getHeight()/2+200, canvas.getWidth()/2+100,canvas.getHeight()/2+200);
-
-        Rect rect4 = new Rect(canvas.getWidth()/2-200, canvas.getHeight()/2, canvas.getWidth()/2-100,canvas.getHeight()/2+100);
-
-        Rect rect5 = new Rect(canvas.getWidth()/2, canvas.getHeight()/2+150, canvas.getWidth()/2+100,canvas.getHeight()/2+300);
-
-        Rect rect6 = new Rect(canvas.getWidth()/2-200, canvas.getHeight()/2+200, canvas.getWidth()/2-100,canvas.getHeight()/2+300);
-
-        if(level>=1)
+        if(level==1){
+            Rect rect = new Rect(canvas.getWidth()/3, canvas.getHeight()/3, 2*(canvas.getWidth()/3),2*(canvas.getHeight()/3));
             canvas.drawBitmap(obj, null, rect, null);
-        if(level>=2)
-            canvas.drawBitmap(obj, null, rect1, null);
-        if(level>=3)
-            canvas.drawBitmap(obj, null, rect2, null);
-        if(level>=4)
-            canvas.drawBitmap(obj, null, rect3, null);
+        }
+        if (level==2){
+            for (int i=0;i<level;i++)
+                canvas.drawBitmap(obj, null, drawRects(level,canvas)[i], null);
+        }
+        if (level==3){
+            for (int i=0;i<level;i++)
+                canvas.drawBitmap(obj, null, drawRects(level,canvas)[i], null);
+        }
+        if (level==4){
+            for (int i=0;i<level;i++)
+                canvas.drawBitmap(obj, null, drawRects(level,canvas)[i], null);
+        }
+        if (level==5){
+            for (int i=0;i<level;i++)
+                canvas.drawBitmap(obj, null, drawRects(level,canvas)[i], null);
+        }
+        if (level==6){
+            for (int i=0;i<level;i++)
+                canvas.drawBitmap(obj, null, drawRects(level,canvas)[i], null);
+        }
+        if (level==7){
+            for (int i=0;i<level;i++)
+                canvas.drawBitmap(obj, null, drawRects(level,canvas)[i], null);
+        }
+        if (level==8){
+            for (int i=0;i<level;i++)
+                canvas.drawBitmap(obj, null, drawRects(level,canvas)[i], null);
+        }
+        if (level==9){
+            for (int i=0;i<level;i++)
+                canvas.drawBitmap(obj, null, drawRects(level,canvas)[i], null);
+        }
+        if (level==-1){
 
-        if(level>=5)
-            canvas.drawBitmap(obj, null, rect4, null);
-
-        if(level>=6)
-            canvas.drawBitmap(obj, null, rect5, null);
-
-        if(level>=7)
-            canvas.drawBitmap(obj, null, rect6, null);
+        }
 
 
 
-//        canvas.drawBitmap(obj, canvas.getWidth()/2, canvas.getHeight()/2, null);
 
 
-//        for (int i=0;i<5;i++) {
-//            for(int j=0;j<5;j++) {
-//                int x = (i / 5) * canvas.getWidth();
-//                int y = (j / 5) * canvas.getHeight() + 150;
-//                Rect rect = new Rect(x, y, (x + canvas.getWidth()/5 ),(y + canvas.getHeight()/5 ));
-//                canvas.drawBitmap(obj, null, rect, null);
-//            }}
 
         // Animation loop (redraw this view by invalidate - call onDraw() - for animation loop
        invalidate();   // temporary method - will be replaced later by Animation Thread
     }
+
+
+    protected Rect[] drawRects(int lev ,Canvas canvas){//places of objects according to levels num
+
+        rect=new Rect[9];//9=max num of objects that can be
+        if (lev==2) {
+            rect[0]=new Rect(canvas.getWidth()/4, canvas.getHeight()/3, 2*(canvas.getWidth()/4),2*(canvas.getHeight()/3));
+            rect[1]=new Rect(2*(canvas.getWidth()/4), canvas.getHeight()/3, 3*(canvas.getWidth()/4),2*(canvas.getHeight()/3));
+        }
+        if (lev==3){
+            rect[0]=new Rect(canvas.getWidth()/4, canvas.getHeight()/4, 2*(canvas.getWidth()/4),2*(canvas.getHeight()/4));
+            rect[1]=new Rect(canvas.getWidth()/4, 2*(canvas.getHeight()/4), 2*(canvas.getWidth()/4),3*(canvas.getHeight()/4));
+            rect[2]=new Rect(2*(canvas.getWidth()/4), 2*(canvas.getHeight()/4), 3*(canvas.getWidth()/4),3*(canvas.getHeight()/4));
+        }
+        if (lev==4){
+            rect[0]=new Rect(canvas.getWidth()/4, canvas.getHeight()/4, 2*(canvas.getWidth()/4),2*(canvas.getHeight()/4));
+            rect[1]=new Rect(2*(canvas.getWidth()/4),canvas.getHeight()/4,3*(canvas.getWidth()/4),2*(canvas.getHeight()/4));
+            rect[2]=new Rect(canvas.getWidth()/4, 2*(canvas.getHeight()/4), 2*(canvas.getWidth()/4),3*(canvas.getHeight()/4));
+            rect[3]=new Rect(2*(canvas.getWidth()/4), 2*(canvas.getHeight()/4), 3*(canvas.getWidth()/4),3*(canvas.getHeight()/4));
+        }
+        if (lev==5){
+            rect[0]=new Rect(30, 100, canvas.getWidth() / 3, canvas.getHeight() / 3 );
+            rect[1]=new Rect(2*(canvas.getWidth() / 3), 100, canvas.getWidth()-30, canvas.getHeight() / 3);
+            rect[2]=new Rect(canvas.getWidth()/3, canvas.getHeight()/3, 2*(canvas.getWidth() / 3)+50,2*(canvas.getHeight()/3));
+            rect[3]=new Rect(30, 2*(canvas.getHeight()/3) , canvas.getWidth() / 3, canvas.getHeight() -100);
+            rect[4]=new Rect(2*(canvas.getWidth() / 3), 2*(canvas.getHeight()/3), canvas.getWidth() -30, canvas.getHeight()-100);
+        }
+        if (lev==6) {
+            rect[0]=new Rect(canvas.getWidth()/4, 90, 2*(canvas.getWidth()/4),1*(canvas.getHeight()/4)+90);
+            rect[1]=new Rect(2*(canvas.getWidth()/4),90,3*(canvas.getWidth()/4),1*(canvas.getHeight()/4)+90);
+            rect[2]=new Rect(canvas.getWidth()/4, 1*(canvas.getHeight()/4)+90, 2*(canvas.getWidth()/4),2*(canvas.getHeight()/4)+90);
+            rect[3]=new Rect(2*(canvas.getWidth()/4), 1*(canvas.getHeight()/4)+90, 3*(canvas.getWidth()/4),2*(canvas.getHeight()/4)+90);
+            rect[4]=new Rect(canvas.getWidth()/4, 2*(canvas.getHeight()/4)+90, 2*(canvas.getWidth()/4),3*(canvas.getHeight()/4)+90);
+            rect[5]=new Rect(2*(canvas.getWidth()/4), 2*(canvas.getHeight()/4)+90, 3*(canvas.getWidth()/4),3*(canvas.getHeight()/4)+90);
+
+        }
+        if (lev==7) {
+            rect[0]=new Rect(30, (canvas.getHeight() / 4), canvas.getWidth() / 5-10, 2*(canvas.getHeight() / 4));
+            rect[1]=new Rect(canvas.getWidth() / 5 + 10, (canvas.getHeight() / 4), 2*(canvas.getWidth() / 5), 2*(canvas.getHeight() / 4));
+            rect[2]=new Rect(3*(canvas.getWidth() / 5), (canvas.getHeight() / 4), 4*(canvas.getWidth() / 5), 2*(canvas.getHeight() / 4));
+            rect[3]=new Rect(30, 2*(canvas.getHeight() / 4), canvas.getWidth() / 5-10, 3*(canvas.getHeight() / 4));
+            rect[4]=new Rect(canvas.getWidth() / 5 + 10, 2*(canvas.getHeight() / 4)+10, 2*(canvas.getWidth() / 5), 3*(canvas.getHeight() / 4));
+            rect[5]=new Rect(3*(canvas.getWidth() / 5), 2*(canvas.getHeight() / 4)+10, 4*(canvas.getWidth() / 5), 3*(canvas.getHeight() / 4) );
+            rect[6]=new Rect(4*(canvas.getWidth() / 5), 2*(canvas.getHeight() / 4)+10, 5*(canvas.getWidth() / 5), 3*(canvas.getHeight() / 4) );
+        }
+        if (lev==8) {
+            rect[0]=new Rect(30, (canvas.getHeight() / 4), canvas.getWidth() / 5-10, 2*(canvas.getHeight() / 4));
+            rect[1]=new Rect(canvas.getWidth() / 5 + 10, (canvas.getHeight() / 4), 2*(canvas.getWidth() / 5), 2*(canvas.getHeight() / 4));
+            rect[2]=new Rect(3*(canvas.getWidth() / 5), (canvas.getHeight() / 4), 4*(canvas.getWidth() / 5), 2*(canvas.getHeight() / 4));
+            rect[3]=new Rect(4*(canvas.getWidth() / 5), (canvas.getHeight() / 4), 5*(canvas.getWidth() / 5), 2*(canvas.getHeight() / 4));
+            rect[4]=new Rect(30, 2*(canvas.getHeight() / 4), canvas.getWidth() / 5-10, 3*(canvas.getHeight() / 4));
+            rect[5]=new Rect(canvas.getWidth() / 5 + 10, 2*(canvas.getHeight() / 4), 2*(canvas.getWidth() / 5), 3*(canvas.getHeight() / 4));
+            rect[6]=new Rect(3*(canvas.getWidth() / 5), 2*(canvas.getHeight() / 4), 4*(canvas.getWidth() / 5), 3*(canvas.getHeight() / 4) );
+            rect[7]=new Rect(4*(canvas.getWidth() / 5), 2*(canvas.getHeight() / 4), 5*(canvas.getWidth() / 5), 3*(canvas.getHeight() / 4) );
+        }
+        if (lev==9) {
+            rect[0]=new Rect(30, (canvas.getHeight() / 5), canvas.getWidth() / 6, 2*(canvas.getHeight() / 5));
+            rect[1]=new Rect(2*(canvas.getWidth() / 6 ), (canvas.getHeight() / 5), 3*(canvas.getWidth() / 6), 2*(canvas.getHeight() / 5));
+            rect[2]=new Rect(4*(canvas.getWidth() / 6), (canvas.getHeight() / 5), 5*(canvas.getWidth() / 6), 2*(canvas.getHeight() / 5));
+            rect[3]=new Rect(5*(canvas.getWidth() / 6), (canvas.getHeight() / 5), 6*(canvas.getWidth() / 6), 2*(canvas.getHeight() / 5));
+            rect[4]=new Rect(canvas.getWidth()/6,2*(canvas.getHeight() / 5),2*(canvas.getWidth()/6),3*(canvas.getHeight() / 5));
+            rect[5]=new Rect(30, 3*(canvas.getHeight() / 5), canvas.getWidth() / 6, 4*(canvas.getHeight() / 5));
+            rect[6]=new Rect(2*(canvas.getWidth() / 6 ), 3*(canvas.getHeight() /5), 3*(canvas.getWidth() / 6), 4*(canvas.getHeight() / 5));
+            rect[7]=new Rect(4*(canvas.getWidth() / 6), 3*(canvas.getHeight() / 5), 5*(canvas.getWidth() / 6), 4*(canvas.getHeight() / 5) );
+            rect[8]=new Rect(5*(canvas.getWidth() / 6), 3*(canvas.getHeight() / 5), 6*(canvas.getWidth() / 6), 4*(canvas.getHeight() / 5) );
+        }
+
+        return rect;
+    }
+
 
 
 
